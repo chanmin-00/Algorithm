fac721c0d19d60375cebacfb2b1c447777772cfa
chanmin <66260209+chanmin-00@users.noreply.github.com>
2023-08-22 14:43:01 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.22 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        int count = 0;
+        
+        for(int i=left;i<right+1;i++){
+            for(int j=1;j<i+1;j++){ // i 약수 세기
+                if(i%j==0){
+                    count++;
+                }
+            }
+            if(count%2==0){
+                answer+=i;
+            }
+            else{
+                answer-=i;
+            }
+            count=0;
+        }
+        return answer;
+    }
+}
