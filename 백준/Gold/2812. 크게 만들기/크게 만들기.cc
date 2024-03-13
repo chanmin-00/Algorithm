ad358915183621ff117c6c0c8d4f76621a763f6f
chanmin <66260209+chanmin-00@users.noreply.github.com>
2024-03-13 18:14:21 +0900

    [Gold III] Title: 크게 만들기, Time: 40 ms, Memory: 5040 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.cc
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.cc
@@ -13,18 +13,18 @@ int main(void)
 	int n;
 	int k;
 	string num;
-
-	stack<int> stk; // k개를 지우고 남아있는 숫자 값을 저장할 스택
+	int count = 0; // 현재 지운 숫자의 개수를 저장하는 변수
+	int currentIndex = 0; // num 문자열에서 현재 가리키고 있는 숫자의 위치를 저장하는 변수
+	stack<int> stk; // k개를 지우고 남아있는 숫자의 위치(인덱스)를 저장할 스택
 
 	cin >> n >> k >> num;
-	int count = 0;
-	int currentIndex = 0;
 
-	while (count < k && currentIndex < n) {
+	while (count < k && currentIndex < n) { // 지운 개수가 k개 보다 작고 currentIndx가 문자열의 범위를 안 넘을 때까지
 		
+		// 스택에 저장된 숫자보다 현재 숫자가 더 클 경우 top의 숫자를 제거 후 push
 		while (!stk.empty() && stoi(string(1, num[stk.top()])) < stoi(string(1, num[currentIndex]))) {
 			stk.pop();
-			count++;
+			count++; // 지운 개수를 증가
 			if (count == k)
 				break;
 		}
@@ -32,19 +32,19 @@ int main(void)
 		stk.push(currentIndex);
 		currentIndex++;
 	}
-
-	for (int i = currentIndex; i < n; i++) {
+ 
+	for (int i = currentIndex; i < n; i++) { // 스택에 저장되지 않은 숫자들 스택에 저장
 		stk.push(i);
 	}
 
 	int* result = new int[stk.size()];
 	int size = stk.size();
 
-	for (int i = size - 1; i > -1; --i) {
+	for (int i = size - 1; i > -1; --i) { // 스택에서 값 추출
 		result[i] = stoi(string(1, num[stk.top()]));
 		stk.pop();
 	}
-	for (int i = 0; i < n - k; i++) {
+	for (int i = 0; i < n - k; i++) { // n - k 개 만큼 결과 출력
 		cout << result[i];
 	}
 	cout << "\n";
