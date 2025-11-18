00d5f9d542ab66d9410ee6e42ba4f6fe75ee696d
chanmin <66260209+chanmin-00@users.noreply.github.com>
2025-11-18 18:40:04 +0900

    [Silver II] Title: 랜선 자르기, Time: 160 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,54 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	/**
-	 * 이미 가지고 있는 랜선을 붙일 수는 없고, 잘라서 만들어야 한다.
-	 * 구하고자 하는 것은, n개를 만들 수 있는 랜선의 최대 길이를 구하느 것이다.
-	 */
-	public static void main(String[] args) throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		String[] input;
+	static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	static String[] input;
 
+	public static void main(String[] args) throws IOException {
 		input = reader.readLine().split(" ");
 
-		int k = Integer.parseInt(input[0]); // 오영식이 이미 가지고 있는 랜선의 개수
-		int n = Integer.parseInt(input[1]); // 필요한 랜선의 개수, 모두 같은 길이의 랜선이 n개 만들어져야 한다.
-		long[] kArr = new long[k];
+		int K = Integer.parseInt(input[0]); // 오영식이 이미 가지고 있는 랜선의 개수
+		int N = Integer.parseInt(input[1]); // 필요한 랜선의 개수
 
-		for (int i = 0; i < k; i++) {
-			long num = Long.parseLong(reader.readLine());
-			kArr[i] = num;
+		int[] arr = new int[K];
+
+		for (int i = 0; i < K; i++) {
+			arr[i] = Integer.parseInt(reader.readLine());
 		}
 
-		Arrays.sort(kArr); // 내림차순으로 정렬한다.
+		Arrays.sort(arr);
 
-		long front = 1;
-		long rear = kArr[k - 1];
-		long middle;
-		long result = 0;
+		long left = 1;
+		long right = arr[K - 1];
+		long mid = 0;
+		long answer = 0;
 
-		// 제일 작은 것을 기준으로 이분탐색을 진행할 것이다.
-		while (front <= rear) {
-			middle = (front + rear) / 2; // 정수 최대 + 정수 최대를 더하면 오버플로 문제가 발생할 수 있다.
+		while (left <= right) {
+			// System.out.println("left : " + left + " right " + right);
 
-			long sum = 0;
-			for (int i = 0; i < k; i++) {
-				sum += kArr[i] / middle;
+			mid = (left + right) / 2;
+			long cnt = 0;
+
+			for (int i = 0; i < K; i++) {
+				cnt += (arr[i] / mid);
 			}
 
-			// 이분 탐색에서는 범위를 -1, +1로 줄여나가야 한다.
-			if (sum < n) {
-				rear = middle - 1; // 조건을 만족하지 않기 때문에, middle 보다 작은 구간부터 검사를 한다.
-			} else { // 조건을 만족한 경우
-				front = middle + 1; // 만족했지만 최대 길이를 구하기 위해 더 긴 길이도 가능한지 확인을 해야 한다.
-				result = middle;
+			if (cnt >= N) { // 이 경우에는 자르는 랜선의 길이를 좀 늘려야 함
+				left = mid + 1;
+				if (answer == mid) {
+					break;
+				}
+				answer = Math.max(answer, mid);
+			} else if (cnt < N) { //  이 경우에는 자르는 랜선의 길이를 좀 줄여야 함
+				right = mid - 1;
 			}
 		}
 
-		System.out.println(result);
+		System.out.println(answer);
+
 	}
 }
